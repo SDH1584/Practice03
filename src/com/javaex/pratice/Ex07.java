be259452863b712111352b5385e689e5e6622691
현 <usesr@DESKTOP-3RO9PAV>
2021-11-22 00:01:21 +0900

    .

--- src/com/javaex/pratice/Ex07.java
+++ src/com/javaex/pratice/Ex07.java
@@ -11,10 +11,10 @@ public class Ex07 {
 		int num=sc.nextInt();
 		
 		for(int i=1; i<=num; i++) {
-
-			
-				System.out.println(i);
-			
+			for(int j=1; j<=i ; j++) {
+				System.out.print(j);
+			}
+			System.out.println("");
 		}
 sc.close();
 }
