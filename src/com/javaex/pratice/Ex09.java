14811df6304d11a9862c24ebb3420a2d40cabd54
현 <usesr@DESKTOP-3RO9PAV>
2021-11-22 10:18:47 +0900

    .

--- src/com/javaex/pratice/Ex09.java
+++ src/com/javaex/pratice/Ex09.java
@@ -3,13 +3,17 @@ package com.javaex.pratice;
 public class Ex09 {
 
 	public static void main(String[] args) {
+		int i;
+		int j;
+		
+		for(i=1; i<=10 ; i++) 
+		{
+			System.out.println(i+"\t");
 			
-		for(int i=1; i<=10 ; i++) {
-			System.out.print(i+"\t");
-	
-		for(int j=i+1; j<=i+10; j++) {
-			System.out.print(j+"\t");
-	}
-}
+			for(j=i+1; j<i+10; j++) 
+			{
+				System.out.print(j+"\t");
+			}
+		}
 	}		
 }
